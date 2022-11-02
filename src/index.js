16843500566b85c56efffa096804322333a3077f
maximBurinskyi <maxim.burinskyi@gmail.com>
2022-11-02 15:40:37 +0200

    other

--- src/index.js
+++ src/index.js
@@ -19,6 +19,12 @@ class Board extends React.Component {
         .fill()
         .map(() => Math.round(Math.random() * 3)),
       xIsNext: true,
+      todos: [
+        'learn react',
+        'learn react props',
+        'learn react state',
+        'drink vodka',
+      ],
     };
   }
 
@@ -34,6 +40,57 @@ class Board extends React.Component {
   //   });
   // }
 
+  sorting(arr) {
+    let result = arr.sort((a, b) => {
+      return a - b;
+    });
+    return result;
+  }
+  count(arr) {
+    Math.max(
+      ...arr.reduce(
+        (acc, n, i, a) => (
+          n !== a[i - 1] && acc.push(0), acc[acc.length - 1]++, acc
+        ),
+        [0]
+      )
+    );
+  }
+
+  count2(arr) {
+    Math.max(
+      ...arr.reduce(
+        (prev, item, index, array) => {
+          return (
+            item !== array[index - 1] && prev.push(0),
+            prev[prev.length - 1]++,
+            prev
+          );
+        },
+        [0]
+      )
+    );
+  }
+
+  count3(arr) {
+    const finalRes = calculateWinner(arr);
+    console.log(finalRes);
+    const arr2 = arr.splice(finalRes[1][0], 3);
+    console.log(arr2);
+    console.log(arr);
+  }
+
+  onTodoDelete(todoText) {
+    //const updatedView = this.state.squares.filter((el) => el !== todoText);
+
+    const newSquares = [...this.state.squares];
+    const updatedView = this.count3(newSquares);
+    console.log(updatedView);
+    this.setState({
+      squares: updatedView,
+    });
+  }
+
   renderSquare(i) {
     const variable = Math.floor(Math.random() * 3) + 1;
     // if (!this.state.squares) {
@@ -47,7 +104,7 @@ class Board extends React.Component {
     return (
       <Square
         value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        onClick={() => this.onTodoDelete(this.state.squares)}
       />
     );
   }
@@ -55,12 +112,17 @@ class Board extends React.Component {
   render() {
     const winner = calculateWinner(this.state.squares);
     console.log(this.state.squares);
-    console.log(winner);
+    //console.log(winner[0]);
 
     let status;
     if (winner) {
       //status = 'Numbers matched in row: ' + winner;
-      status = `Numbers under number ${winner} have matched in a row`;
+      status = `Numbers under number ${winner[0]} have matched in a row and we deleted them. You need to refresh the page now to see new numbers`;
+      const finalRes = winner;
+      console.log(finalRes);
+      const arr2 = this.state.squares.splice(finalRes[1][0], 3);
+      console.log(arr2);
+      //console.log(arr);
     } else {
       status = 'Please try again to refresh the page: ';
       //  + (this.state.xIsNext ? 'X' : 'O');
@@ -84,7 +146,7 @@ class Board extends React.Component {
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
-        <button onClick={this.handleClick}> refresh</button>
+        <button onClick={this.onTodoDelete}> refresh</button>
       </div>
     );
   }
@@ -112,6 +174,16 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Game />);
 
 function calculateWinner(squares) {
+  // const lines = [
+  //   [0, 1, 2],
+  //   [3, 4, 5],
+  //   [6, 7, 8],
+  //   [0, 3, 6],
+  //   [1, 4, 7],
+  //   [2, 5, 8],
+  //   [0, 4, 8],
+  //   [2, 4, 6],
+  // ];
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -119,13 +191,13 @@ function calculateWinner(squares) {
     [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
+    // [0, 4, 8],
+    // [2, 4, 6],
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return [squares[a], lines[i]];
     }
   }
   return null;
